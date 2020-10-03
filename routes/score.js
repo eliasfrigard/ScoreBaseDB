d45b63b212c275b32a6c7189268e287bb481e69b
Elias Frigård <ef222xf@student.lnu.se>
2020-10-03 16:22:55 +0300

    log

--- routes/score.js
+++ routes/score.js
@@ -137,7 +137,7 @@ router.get('/recent', async (request, response) => {
 })
 
 router.post('/file', async (request, response) => {
-  console.log(request);
+  console.log(request.body.files);
   try {
     if (!request.files.get('score')) {
       response.send({

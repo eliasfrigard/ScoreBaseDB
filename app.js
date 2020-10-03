ba54e8e54aac924579a5261f2317d5ad85d7bccd
Elias Frigård <ef222xf@student.lnu.se>
2020-10-03 16:33:56 +0300

    log

--- app.js
+++ app.js
@@ -46,3 +46,33 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
 
 // Listeing to port:
 application.listen(8080)
+
+application.post('/upload-avatar', async (req, res) => {
+  try {
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'No file uploaded'
+          });
+      } else {
+          //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+          let avatar = req.files.avatar;
+          
+          //Use the mv() method to place the file in upload directory (i.e. "uploads")
+          avatar.mv('./static/' + avatar.name);
+
+          //send response
+          res.send({
+              status: true,
+              message: 'File is uploaded',
+              data: {
+                  name: avatar.name,
+                  mimetype: avatar.mimetype,
+                  size: avatar.size
+              }
+          });
+      }
+  } catch (err) {
+      res.status(500).send(err);
+  }
+});
--- views/uploading/upload.js
+++ views/uploading/upload.js
@@ -118,6 +118,22 @@ const upload = file => {
   )
 }
 
+const uploadFile = (file) => {
+
+  // add file to FormData object
+  const fd = new FormData();
+  fd.append('avatar', file);
+
+  // send `POST` request
+  fetch('/upload-avatar', {
+      method: 'POST',
+      body: fd
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
+}
+
 const countries = [
   "sweden",
   "finland",

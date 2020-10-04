12e8f6a1a84e579095f28fab9b6f1b4007a6d171
Elias Frigård <ef222xf@student.lnu.se>
2020-10-04 13:21:55 +0300

    moved to upload route

--- routes/upload.js
+++ routes/upload.js
@@ -3,15 +3,15 @@ const router = express.Router()
 
 // Serving upload page, move later.
 router.get('/', async (req, res) => {
-  res.sendFile(__dirname + '/views/upload/index.html')
+  res.sendFile('../' + __dirname + '/views/upload/index.html')
 })
 
 router.get('/style', async (req, res) => {
-  res.sendFile(__dirname + '/views/upload/style.css')
+  res.sendFile('../' + __dirname + '/views/upload/style.css')
 })
 
 router.get('/js', async (req, res) => {
-  res.sendFile(__dirname + '/views/upload/upload.js')
+  res.sendFile('../' + __dirname + '/views/upload/upload.js')
 })
 
 module.exports = router
